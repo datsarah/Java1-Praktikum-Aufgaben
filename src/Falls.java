b5ce7f31a971c345a1c77c0ba239a2e7c9b9da9c
datsarah <96551259+datsarah@users.noreply.github.com>
2022-11-06 14:30:28 +0100

    finsh

---
+++ src/Falls.java
@@ -0,0 +1,14 @@
+public class Falls {
+    public static void main(String[] args) {
+        int i = 50;
+
+        if(i < 100){
+            System.out.println("Wert ist unterhalb von 100");
+            if((i / 5) == 10){
+                System.out.println("Wert geteilt durch 5 ergibt 10");
+            }
+        } else {
+            System.out.println("Wert ist mindestens 100");
+        }
+    }
+}
