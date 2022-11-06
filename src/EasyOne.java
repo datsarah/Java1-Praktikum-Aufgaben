9af429eeeb8cb0e2488e540a532755a91a1c255c
datsarah <96551259+datsarah@users.noreply.github.com>
2022-11-06 14:29:30 +0100

    finsh

---
+++ src/EasyOne.java
@@ -0,0 +1,13 @@
+public class EasyOne {
+    public static void main(String[] args) {
+        String eins = "1", zwei = "2";
+
+        int one = 1, two = 2;
+
+        //Ausgabe: eins+zwei, one+two, eins+one
+        System.out.println(eins + zwei);
+        System.out.println(one + two);
+        System.out.println(eins + one);
+
+    }
+}
